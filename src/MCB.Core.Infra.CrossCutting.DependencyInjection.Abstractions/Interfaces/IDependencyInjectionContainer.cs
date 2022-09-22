4e18a466185d1c0961ab8984de73c91f681a8cae
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-09-22 19:50:33 -0300

    feat: add nullable check

--- src/MCB.Core.Infra.CrossCutting.DependencyInjection.Abstractions/Interfaces/IDependencyInjectionContainer.cs
+++ src/MCB.Core.Infra.CrossCutting.DependencyInjection.Abstractions/Interfaces/IDependencyInjectionContainer.cs
@@ -9,35 +9,35 @@ public interface IDependencyInjectionContainer
     #endregion
 
     #region [ Resolve ]
-    object Resolve(Type type);
-    TType Resolve<TType>();
+    object? Resolve(Type type);
+    TType? Resolve<TType>();
     #endregion
 
     #region [ Register ]
     void Register(DependencyInjectionLifecycle lifecycle, Type concreteType);
-    void Register(DependencyInjectionLifecycle lifecycle, Type concreteType, Func<IDependencyInjectionContainer, object> concreteTypeFactory);
+    void Register(DependencyInjectionLifecycle lifecycle, Type concreteType, Func<IDependencyInjectionContainer, object?> concreteTypeFactory);
     void Register(DependencyInjectionLifecycle lifecycle, Type abstractionType, Type concreteType);
-    void Register(DependencyInjectionLifecycle lifecycle, Type abstractionType, Type concreteType, Func<IDependencyInjectionContainer, object> concreteTypeFactory);
+    void Register(DependencyInjectionLifecycle lifecycle, Type abstractionType, Type concreteType, Func<IDependencyInjectionContainer, object?> concreteTypeFactory);
 
     void Register<TConcreteType>(DependencyInjectionLifecycle lifecycle);
-    void Register<TConcreteType>(DependencyInjectionLifecycle lifecycle, Func<IDependencyInjectionContainer, TConcreteType> concreteTypeFactory);
-    void Register<TAbstractionType, TConcreteType>(DependencyInjectionLifecycle lifecycley);
-    void Register<TAbstractionType, TConcreteType>(DependencyInjectionLifecycle lifecycle, Func<IDependencyInjectionContainer, TConcreteType> concreteTypeFactory);
+    void Register<TConcreteType>(DependencyInjectionLifecycle lifecycle, Func<IDependencyInjectionContainer, TConcreteType?> concreteTypeFactory);
+    void Register<TAbstractionType, TConcreteType>(DependencyInjectionLifecycle lifecycle);
+    void Register<TAbstractionType, TConcreteType>(DependencyInjectionLifecycle lifecycle, Func<IDependencyInjectionContainer, TConcreteType?> concreteTypeFactory);
 
     void RegisterTransient<TConcreteType>();
-    void RegisterTransient<TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType> concreteTypeFactory);
+    void RegisterTransient<TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType?> concreteTypeFactory);
     void RegisterTransient<TAbstractionType, TConcreteType>();
-    void RegisterTransient<TAbstractionType, TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType> concreteTypeFactory);
+    void RegisterTransient<TAbstractionType, TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType?> concreteTypeFactory);
 
     void RegisterScoped<TConcreteType>();
-    void RegisterScoped<TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType> concreteTypeFactory);
+    void RegisterScoped<TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType?> concreteTypeFactory);
     void RegisterScoped<TAbstractionType, TConcreteType>();
-    void RegisterScoped<TAbstractionType, TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType> concreteTypeFactory);
+    void RegisterScoped<TAbstractionType, TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType?> concreteTypeFactory);
 
     void RegisterSingleton<TConcreteType>();
-    void RegisterSingleton<TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType> concreteTypeFactory);
+    void RegisterSingleton<TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType?> concreteTypeFactory);
     void RegisterSingleton<TAbstractionType, TConcreteType>();
-    void RegisterSingleton<TAbstractionType, TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType> concreteTypeFactory);
+    void RegisterSingleton<TAbstractionType, TConcreteType>(Func<IDependencyInjectionContainer, TConcreteType?> concreteTypeFactory);
     #endregion
 
     #region [ Unregister ]
